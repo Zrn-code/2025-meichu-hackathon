bb4cc9907f13700bc9545cdf064f05bf5d8253e6
AnDeSiTe6001 <josephlee921006@gmail.com>
2025-09-19 21:39:03 +0800

    新增 Avatar換頭像

--- windows-app/src/Avatar.jsx
+++ windows-app/src/Avatar.jsx
@@ -1,13 +1,13 @@
 import './index.css';
 
-const Avatar = () => {
 
+const Avatar = ({ src }) => {
   return (
     <div className="flex items-center justify-center w-full h-full overflow-hidden">
       {/* Avatar 圖片 */}
       <div className="w-16 h-16 rounded-full border-2 border-white/80 shadow-lg overflow-hidden bg-gray-100 avatar-drag cursor-move">
         <img 
-          src="/avatar.jpg" 
+          src={src}
           alt="Avatar" 
           className="w-full h-full object-cover rounded-full"
         />
